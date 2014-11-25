1848ef354377f958a68445b8ec18f0d1611069d4
achingbrain <alex@achingbrain.net>
2014-11-25 17:31:49 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var http = require('http'),
+  moment = require('moment'),
+  port = process.env.PORT || 9000
+
+http.createServer(function(req, res) {
+  console.info('incoming request for version 1 at %s!', moment().format('MMMM Do YYYY, h:mm:ss a'))
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('DERP DERP')
+}).listen(port)
+
+console.log('Server listening on %d - pid %d', port, process.pid)
