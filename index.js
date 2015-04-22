474beb5fbe4fd038610731a2762fb4955f7f8e6f
achingbrain <alex@achingbrain.net>
2015-04-22 21:07:11 +0100

    better derping

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var http = require('http'),
   port = process.env.PORT || 9000
 
 http.createServer(function(req, res) {
-  console.info('incoming request for version 3 at %s!', moment().format('MMMM Do YYYY, h:mm:ss a'))
+  console.info('incoming request for version 4 at %s!', moment().format('MMMM Do YYYY, h:mm:ss a'))
   res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('DERP DERP')
+  res.end('DERP DERP v4')
 }).listen(port)
 
 console.log('Server listening on %d - pid %d', port, process.pid)
