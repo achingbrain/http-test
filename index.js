1d3fa0d1c982bcdfef4edcd96a811284998b1862
achingbrain <alex@achingbrain.net>
2015-04-24 13:41:18 +0100

    totally working

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http'),
   moment = require('moment'),
   port = process.env.PORT || 9000
 
-var version = 13
+var version = 14
 
 http.createServer(function(req, res) {
   console.info('incoming request for version %d at %s!', version, moment().format('MMMM Do YYYY, h:mm:ss a'))
