c274993d8db9c0978dd947a00edb4dce0562e702
achingbrain <alex@achingbrain.net>
2015-04-24 07:52:40 +0100

    hmm

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var http = require('http'),
   moment = require('moment'),
   port = process.env.PORT || 9000
 
+var version = 6
+
 http.createServer(function(req, res) {
-  console.info('incoming request for version 4 at %s!', moment().format('MMMM Do YYYY, h:mm:ss a'))
+  console.info('incoming request for version %d at %s!', version, moment().format('MMMM Do YYYY, h:mm:ss a'))
   res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('DERP DERP v5')
+  res.end('DERP DERP v' + version)
 }).listen(port)
 
 console.log('Server listening on %d - pid %d', port, process.pid)
