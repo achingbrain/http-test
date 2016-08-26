f255e1dbe5b7d88ed4da753f58cc228eb19e5f2b
Alex Potsides <alex.potsides@mendeley.com>
2016-08-26 16:57:09 +0100

    Choose random high port

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var http = require('http'),
-  moment = require('moment'),
-  port = process.env.PORT || 9000
+  moment = require('moment')
+  freeport = require('freeport')
 
-var version = 'branch'
+freeport((error, port) => {
+  var version = 'branch'
 
-http.createServer(function(req, res) {
-  console.info('incoming request for version %d at %s!', version, moment().format('MMMM Do YYYY, h:mm:ss a'))
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('DERP DERP v' + version)
-}).listen(port)
+  http.createServer(function(req, res) {
+    console.info('incoming request for version %d at %s!', version, moment().format('MMMM Do YYYY, h:mm:ss a'))
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end('DERP DERP v' + version)
+  }).listen(port)
 
-console.log('Server listening on %d - pid %d', port, process.pid)
+  console.log('Server listening on %d - pid %d', port, process.pid)
+})
